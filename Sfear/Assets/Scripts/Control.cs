044a173cfe771a77832fe66ba66f12753db94967
Ktcmoop <Ktcmoop@hotmail.com>
2018-06-02 01:27:32 -0700

    Basic Movement
    +Added sphere in center of scene
    +Added player capsule
    +Added control script, move by clicking and dragging
    
    TODO: Add networking

---
+++ Sfear/Assets/Scripts/Control.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour {
+	private GameObject ball;			//Reference to the Sfear
+	private Vector3 relativePosition;	//Position of this player relative to the center of the Sfear
+	private float radius;				//Radius of ball PLUS half height of capsule
+	private float hspeed, vspeed;		//Horizontal and vertical speed (relative to camera plane)
+	
+	//Public variables
+	public float acceleration = 0.0005f;	//How fast the player object responds to swiping
+	public float maxSpeed = 0.015f;		//Fastest possible move speed
+	public float slowDown = 0.99f;		//Multiplied by movement speed every step to make smooth movement
+	
+	void Start () {
+		radius = 0.55f;
+		
+		//Get a reference to the ball
+		ball = GameObject.FindWithTag("Ball"); 
+		
+		//Find relative position to sphere's center
+		relativePosition = transform.position - ball.transform.position;
+		
+		Camera.main.transform.position = transform.position + relativePosition;
+		Camera.main.transform.LookAt(ball.transform, Camera.main.transform.up);
+	}
+	
+	void Update () {
+		/*
+		if(!isLocalPlayer){
+			return;
+		}
+		*/
+		
+		//Slow down gradually
+		hspeed *= slowDown;
+		vspeed *= slowDown;
+		
+		//If user's finger is held down, calculate move speed.
+		if(Input.GetAxis("Tap") == 1){
+			//add mouse movement to this player's speed
+			hspeed -= Input.GetAxis("Mouse X")*acceleration;
+			vspeed -= Input.GetAxis("Mouse Y")*acceleration;
+			
+			//Clamp to max speed
+			hspeed = Mathf.Clamp(hspeed, -maxSpeed, maxSpeed);
+			vspeed = Mathf.Clamp(vspeed, -maxSpeed, maxSpeed);
+		}
+		
+		//Move
+		move(hspeed,vspeed);
+		
+		//Find relative position to sphere's center
+		relativePosition = transform.position - ball.transform.position;
+		
+		//Move camera to be above self, relative to the ball
+		Camera.main.transform.position = transform.position + relativePosition;
+
+		//Look down towards sphere
+		Camera.main.transform.LookAt(ball.transform, Camera.main.transform.up);
+		
+		//Rotate player model accordingly
+		transform.rotation = Camera.main.transform.rotation;
+		transform.Rotate(90,0,0);
+	}
+	
+	void move(float h, float v){
+		//Move in a tangent line (Because camera plane is tangent to sphere)
+		transform.position += Camera.main.transform.right*h;
+		transform.position += Camera.main.transform.up*v;
+		
+		//Snap back to sphere's surface
+		transform.position = transform.position.normalized*radius;
+	}
+}
