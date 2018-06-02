f3b50d60fd5522c8374484f0465613ca62f98721
Melody Chung <melodc2@uci.edu>
2018-06-02 16:49:46 -0700

    Added Pre-Round and Round Countdown Timer
    - I pretty much just took the timer code from Black Friday Simulator for this

--- Sfear/Assets/Scripts/Control.cs
+++ Sfear/Assets/Scripts/Control.cs
@@ -8,9 +8,11 @@ public class Control : NetworkBehaviour {
 	private Vector3 relativePosition;	//Position of this player relative to the center of the Sfear
 	private float radius;				//Radius of ball PLUS half height of capsule
 	private float hspeed, vspeed;		//Horizontal and vertical speed (relative to camera plane)
-	
-	//Public variables
-	public float acceleration = 0.0005f;	//How fast the player object responds to swiping
+    [SyncVar]
+    private bool canMove = true;
+
+    //Public variables
+    public float acceleration = 0.0005f;	//How fast the player object responds to swiping
 	public float maxSpeed = 0.015f;		//Fastest possible move speed
 	public float slowDown = 0.99f;		//Multiplied by movement speed every step to make smooth movement
 
@@ -38,39 +40,43 @@ public class Control : NetworkBehaviour {
 			return;
 		}
 
-		//Used to keep old settings while making movement framerate-independent
-		//float dT = Time.deltaTime*60;
-        		
-		//Slow down gradually
-		hspeed *= slowDown/**d*T*/;
-		vspeed *= slowDown/**dT*/;
-		
-		//If user's finger is held down, calculate move speed.
-		if(Input.GetAxis("Tap") == 1){
-			//add mouse movement to this player's speed
-			hspeed -= Input.GetAxis("Mouse X")*acceleration/**dT*/;
-			vspeed -= Input.GetAxis("Mouse Y")*acceleration/*dT*/;
-			
-			//Clamp to max speed
-			hspeed = Mathf.Clamp(hspeed, -maxSpeed, maxSpeed);
-			vspeed = Mathf.Clamp(vspeed, -maxSpeed, maxSpeed);
-		}
-		
-		//Move
-		Move(hspeed,vspeed);
-		
-		//Find relative position to sphere's center
-		relativePosition = transform.position - ball.transform.position;
-		
-		//Move camera to be above self, relative to the ball
-		Camera.main.transform.position = transform.position + relativePosition;
+        if (canMove)
+        {
+            //Used to keep old settings while making movement framerate-independent
+            //float dT = Time.deltaTime*60;
 
-		//Look down towards sphere
-		Camera.main.transform.LookAt(ball.transform, Camera.main.transform.up);
-		
-		//Rotate player model accordingly
-		transform.rotation = Camera.main.transform.rotation;
-		transform.Rotate(90,0,0);
+            //Slow down gradually
+            hspeed *= slowDown/**d*T*/;
+            vspeed *= slowDown/**dT*/;
+
+            //If user's finger is held down, calculate move speed.
+            if (Input.GetAxis("Tap") == 1)
+            {
+                //add mouse movement to this player's speed
+                hspeed -= Input.GetAxis("Mouse X") * acceleration/**dT*/;
+                vspeed -= Input.GetAxis("Mouse Y") * acceleration/*dT*/;
+
+                //Clamp to max speed
+                hspeed = Mathf.Clamp(hspeed, -maxSpeed, maxSpeed);
+                vspeed = Mathf.Clamp(vspeed, -maxSpeed, maxSpeed);
+            }
+
+            //Move
+            Move(hspeed, vspeed);
+
+            //Find relative position to sphere's center
+            relativePosition = transform.position - ball.transform.position;
+
+            //Move camera to be above self, relative to the ball
+            Camera.main.transform.position = transform.position + relativePosition;
+
+            //Look down towards sphere
+            Camera.main.transform.LookAt(ball.transform, Camera.main.transform.up);
+
+            //Rotate player model accordingly
+            transform.rotation = Camera.main.transform.rotation;
+            transform.Rotate(90, 0, 0);
+        }
 	}
 	
 	void Move(float h, float v){
@@ -114,5 +120,10 @@ public class Control : NetworkBehaviour {
         Debug.Log("Free Game");
         canBeTagged = true;
         GetComponent<MeshRenderer>().material.color = Color.white;
+    }
+
+    public void SetCanMove(bool b)
+    {
+        canMove = b;
     }
 }
---
+++ Sfear/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,110 @@
+﻿/* Taken from https://answers.unity.com/questions/1179602/implementing-server-side-code-with-unet.html and modified.
+ This script handles the round timers for the game. First, there is a 5 second pre-round countdown where players cannot move.
+ Afterwards, the round actually begins, and players are free to move.*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class GameTimer : NetworkBehaviour
+{
+    private const float roundTime = 30.0f; //How long each round of the game should be
+    private const float countDownTime = 6.0f; //The pre-round countdown timer
+    [SyncVar]
+    public float gameTime; //The length of a game, in seconds.
+    [SyncVar]
+    public float timer; //How long the game has been running.
+    [SyncVar]
+    public bool masterTimer = false; //Is this the master timer?
+    [SyncVar]
+    public bool roundHasStarted; //Whether the game has started or not
+
+    public Text countdownTimerText;
+
+    GameTimer serverTimer;
+
+    void Start()
+    {
+        roundHasStarted = false;
+        timer = countDownTime;
+        if (isServer)
+        { // For the host to do: use the timer and control the time.
+            if (isLocalPlayer)
+            {
+                serverTimer = this;
+                masterTimer = true;
+            }
+        }
+        else if (isLocalPlayer)
+        { //For all the boring old clients to do: get the host's timer.
+            GameTimer[] timers = FindObjectsOfType<GameTimer>();
+            for (int i = 0; i < timers.Length; i++)
+            {
+                if (timers[i].masterTimer)
+                {
+                    serverTimer = timers[i];
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+        //Debug.Log("Round has started: " + roundHasStarted + "for player " + GetComponent<PlayerResources>().GetId());
+        if (!roundHasStarted)
+        {
+            GetComponent<Control>().SetCanMove(false);
+
+            if (timer < 0)
+            {
+                Debug.Log("Round is starting.");
+                StartCoroutine(RoundStartPause(3f));
+                timer = roundTime;
+                roundHasStarted = true;
+                GetComponent<Control>().SetCanMove(true);
+            }
+        }
+
+        if (masterTimer)
+        { //Only the MASTER timer controls the time
+            timer -= Time.deltaTime;
+            //Debug.Log(timer);
+            if(roundHasStarted && timer<= 0)
+            {
+                timer = 0;
+            }
+        }
+
+        if (isLocalPlayer)
+        { //EVERYBODY updates their own time accordingly.
+            if (serverTimer)
+            {
+                gameTime = serverTimer.gameTime;
+                timer = serverTimer.timer;
+                countdownTimerText.text = Mathf.Floor(timer).ToString();
+            }
+            else
+            { //Maybe we don't have it yet?
+                GameTimer[] timers = FindObjectsOfType<GameTimer>();
+                for (int i = 0; i < timers.Length; i++)
+                {
+                    if (timers[i].masterTimer)
+                    {
+                        serverTimer = timers[i];
+                    }
+                }
+            }
+            if(roundHasStarted && timer<=0)
+            {
+                GetComponent<Control>().SetCanMove(false);
+            }
+        }
+    }
+
+    IEnumerator RoundStartPause(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+    }
+}
